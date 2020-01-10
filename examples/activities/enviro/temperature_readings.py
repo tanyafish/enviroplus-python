377502f901daaf7a2094dd6c3be8bfd1ffa112ba
Tanya Fish <tanyafxg@gmail.com>
2020-01-10 13:03:38 +0000

    Update temperature_readings.py

--- examples/activities/enviro/temperature_readings.py
+++ examples/activities/enviro/temperature_readings.py
@@ -1,6 +1,6 @@
-# uses nicenames instead of sensor references (eg weather not bme280)
+# uses short names instead of sensor references (eg weather not bme280)
 
-import nicenames
+import short
 import time
 
 # takes a temperature reading from the bme280 (weather) and calls it "temp"
@@ -8,6 +8,6 @@ import time
 # waits 1 second before doing it again
 
 while True:
-    temp = nicenames.weather.get_temperature()
+    temp = short.weather.get_temperature()
     print(temp)
     time.sleep(1)
